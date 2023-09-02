f21d057ac9aa78d6a2d072a7d0c4f0795f36c093
Kenta Tanii <t18cs030.y@gmail.com>
2023-09-02 18:47:31 +0900

    a-rotate回答

---
+++ 2022-01-15/a-rotate.py
@@ -0,0 +1,9 @@
+# このファイルはAtCoderの練習用です
+
+num=int(input())
+a= num//100
+b= num%100//10
+c= num%10
+
+
+print(111*(a+b+c))
--- cal.py
+++
@@ -1,8 +0,0 @@
-a=1;b=2;c=3;
-
-print(111*(a+b+c))
-
-print("hello world")
-
-def draw():
-    print("sex")
